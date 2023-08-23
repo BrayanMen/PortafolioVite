6da09bc0d476578e025f12d6529970e33a98bfef
BrayanMen <95663591+BrayanMen@users.noreply.github.com>
2023-08-23 15:33:17 -0300

    2 files modified

--- src/Component/AboutMe.jsx
+++ src/Component/AboutMe.jsx
@@ -1,12 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './AboutMe.module.scss';
 import { useLocation } from 'react-router-dom';
 
-export default function AboutMe({setActiveSection}) {
-  useEffect(()=>{
-    setActiveSection('about-me')
-  },[setActiveSection])
+export default function AboutMe({ setActiveSection }) {
+  const [age, setAge] = useState(0);
+  const [programming, setProgramming] = useState(0);
+
   const location = useLocation();
+
+  const birthDate = new Date(1995, 4, 8);
+  const calculateAge = () => {
+    const date = new Date();
+    const diff = date.getFullYear() - birthDate.getFullYear();
+    const birthdayOccurred =
+      date.getMonth() > birthDate.getMonth() ||
+      (date.getMonth() === birthDate.getMonth() &&
+        date.getDate() >= birthDate.getDate());
+    const calculated = birthdayOccurred ? diff : diff - 1;
+    setAge(calculated);
+
+    const programmingStartDate = new Date(2021, 11, 15); 
+      const timeDiff = Math.abs(date - programmingStartDate);
+      const yearsProgrammed = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
+      setProgramming(yearsProgrammed);
+  };
+
+  useEffect(() => {
+    setActiveSection('about-me');
+    
+    calculateAge();
+    const interval = setInterval(() => {
+      calculateAge();
+    }, 1000 * 60 * 60 * 24);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [setActiveSection]);
 
   return (
     <section className={styles.aboutContainer} id="about-me">
@@ -16,9 +46,33 @@ export default function AboutMe({setActiveSection}) {
       <div className={styles.divAbout}>
         <h1 className="titleAbout">Sobre mi:</h1>
         <p className="pAbput">
-        Apasionado desarrollador web con creatividad y pasión por la tecnología. Creo soluciones funcionales y únicas, fusionando habilidades técnicas con innovación. Siempre en búsqueda de nuevas tendencias y tecnologías para ofrecer resultados de calidad. Busco proyectos desafiantes para transformar ideas en realidad.{' '}
+          Apasionado desarrollador web con creatividad y pasión por la
+          tecnología. Creo soluciones funcionales y únicas, fusionando
+          habilidades técnicas con innovación. Siempre en búsqueda de nuevas
+          tendencias y tecnologías para ofrecer resultados de calidad. Busco
+          proyectos desafiantes para transformar ideas en realidad.{' '}
         </p>
-        
+        <ul className={styles.aboutList}>
+          <li className={styles.aboutItem}>
+            <strong>Edad: </strong>{age}
+          </li>
+          <li className={styles.aboutItem}>
+            <strong>Telefono:</strong> +54 9 11 2730 0038
+          </li>
+          <li className={styles.aboutItem}>
+            <strong>Residencia:</strong> Argentina
+          </li>
+          <li className={styles.aboutItem}>
+            <strong>E-mail:</strong>
+            <a> brayanjmr880@gmail.com</a>
+          </li>
+          <li className={styles.aboutItem}>
+            <strong>Direccion:</strong> Buenos Aires, Avellaneda.
+          </li>
+          <li className={styles.aboutItem}>
+            <strong>Tiempo Programando: </strong>{programming}
+          </li>
+        </ul>
       </div>
     </section>
   );
