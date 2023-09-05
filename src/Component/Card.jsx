b0658ae84095661ee2a767ec683e612aeb2107f2
BrayanMen <95663591+BrayanMen@users.noreply.github.com>
2023-09-05 20:50:13 -0300

    2 files modified

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -3,6 +3,8 @@ import styles from './Card.module.scss';
 
 export default function Card(){
 return (
-  <div></div>
+  <div>
+    
+  </div>
 )
 }
---
+++ src/Data/projects.jsx
@@ -0,0 +1,111 @@
+import antDesign from '../assets/Images/Icons/antdesign.png';
+import bootstrap from '../assets/Images/Icons/bootstrap.png';
+import css from '../assets/Images/Icons/css3.png';
+import express from '../assets/Images/Icons/express.png';
+import html from '../assets/Images/Icons/html5.png';
+import js from '../assets/Images/Icons/javascript.png';
+import nodejs from '../assets/Images/Icons/nodejs.png';
+import postgre from '../assets/Images/Icons/postgresql-wordmark.png';
+import reactjs from '../assets/Images/Icons/react.png';
+import redux from '../assets/Images/Icons/redux.png';
+import sequelize from '../assets/Images/Icons/sequelize.png';
+
+const projectsData = [
+  {
+    title: 'Museo Patagonico de Ciencias Naturales "Juan Carlos Salgado"',
+    summary: '',
+    technologies: {
+      name: 'Javascript',
+      image: `${js}`,
+    },
+    deployLink: '',
+    image: '',
+  },
+  {
+    title: 'Food API',
+    summary: '',
+    technologies: {
+      name: 'Javascript',
+      image: `${js}`,
+    },
+    deployLink: '',
+    image: '',
+  },
+  {
+    title: 'ToDo List',
+    summary: '',
+    technologies: {
+      name: 'Javascript',
+      image: `${js}`,
+    },
+    deployLink: '',
+    image: '',
+  },
+  {
+    title: 'Movie Api Js',
+    summary: '',
+    technologies: {
+      name: 'Javascript',
+      image:`${js}`,
+    },
+    deployLink: '',
+    image: '',
+  },
+  {
+    title: 'Weather App',
+    summary: '',
+    technologies: {
+      name: 'Javascript',
+      image: `${js}`,
+    },
+    deployLink: '',
+    image: '',
+  },
+];
+
+const filter = [
+  {
+    name: 'Javascript',
+    image: `${js}`,
+  },
+  {
+    name: 'HTML',
+    image: `${html}`,
+  },
+  {
+    name: 'CSS',
+    image: `${css}`,
+  },
+  {
+    name: 'React JS',
+    image: `${reactjs}`,
+  },
+  {
+    name: 'Redux',
+    image: `${redux}`,
+  },
+  {
+    name: 'Node JS',
+    image: `${nodejs}`,
+  },
+  {
+    name: 'Express JS',
+    image: `${express}`,
+  },
+  {
+    name: 'Ant Design',
+    image: `${antDesign}`,
+  },
+  {
+    name: 'Bootstrap',
+    image: `${bootstrap}`,
+  },
+  {
+    name: 'PostgreSQL',
+    image: `${postgre}`,
+  },
+  {
+    name: 'Sequelize JS',
+    image: `${sequelize}`,
+  },
+];
