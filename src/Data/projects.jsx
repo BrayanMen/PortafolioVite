29ce1ee6ac8eb3c06b3d44c207e08cd42415fdd3
BrayanMen <95663591+BrayanMen@users.noreply.github.com>
2023-09-06 19:31:25 -0300

    1 binaries uploaded

--- src/Data/projects.jsx
+++ src/Data/projects.jsx
@@ -5,7 +5,6 @@ import express from '../assets/Images/Icons/express.png';
 import html from '../assets/Images/Icons/html5.png';
 import js from '../assets/Images/Icons/javascript.png';
 import nodejs from '../assets/Images/Icons/nodejs.png';
-import postgre from '../assets/Images/Icons/postgresql-wordmark.png';
 import reactjs from '../assets/Images/Icons/react.png';
 import redux from '../assets/Images/Icons/redux.png';
 import sequelize from '../assets/Images/Icons/sequelize.png';
@@ -14,50 +13,140 @@ const projectsData = [
   {
     title: 'Museo Patagonico de Ciencias Naturales "Juan Carlos Salgado"',
     summary: '',
-    technologies: {
-      name: 'Javascript',
-      image: `${js}`,
-    },
+    technologies: [
+      {
+        name: 'Javascript',
+        image: `${js}`,
+      },
+      {
+        name: 'React JS',
+        image: `${reactjs}`,
+      },
+      {
+        name: 'Redux',
+        image: `${redux}`,
+      },
+      {
+        name: 'Bootstrap',
+        image: `${bootstrap}`,
+      },
+      {
+        name: 'Ant Design',
+        image: `${antDesign}`,
+      },
+      {
+        name: 'Node JS',
+        image: `${nodejs}`,
+      },
+      {
+        name: 'Express JS',
+        image: `${express}`,
+      },
+      {
+        name: 'Sequelize',
+        image: `${sequelize}`,
+      },
+    ],
     deployLink: '',
     image: '',
   },
   {
     title: 'Food API',
     summary: '',
-    technologies: {
-      name: 'Javascript',
-      image: `${js}`,
-    },
+    technologies: [
+      {
+        name: 'Javascript',
+        image: `${js}`,
+      },
+      {
+        name: 'React JS',
+        image: `${reactjs}`,
+      },
+      {
+        name: 'Redux',
+        image: `${redux}`,
+      },
+      {
+        name: 'Node JS',
+        image: `${nodejs}`,
+      },
+      {
+        name: 'Express JS',
+        image: `${express}`,
+      },
+      {
+        name: 'Sequelize',
+        image: `${sequelize}`,
+      },
+    ],
     deployLink: '',
     image: '',
   },
   {
     title: 'ToDo List',
     summary: '',
-    technologies: {
-      name: 'Javascript',
-      image: `${js}`,
-    },
+    technologies: [
+      {
+        name: 'Javascript',
+        image: `${js}`,
+      },
+      {
+        name: 'React JS',
+        image: `${reactjs}`,
+      },
+    ],
     deployLink: '',
     image: '',
   },
   {
     title: 'Movie Api Js',
     summary: '',
-    technologies: {
-      name: 'Javascript',
-      image:`${js}`,
-    },
+    technologies: [
+      {
+        name: 'Javascript',
+        image: `${js}`,
+      },
+      {
+        name: 'HTML',
+        image: `${html}`,
+      },
+      {
+        name: 'CSS',
+        image: `${css}`,
+      },
+    ],
     deployLink: '',
     image: '',
   },
   {
     title: 'Weather App',
     summary: '',
-    technologies: {
-      name: 'Javascript',
-      image: `${js}`,
-    },
+    technologies: [
+      {
+        name: 'Javascript',
+        image: `${js}`,
+      },
+      {
+        name: 'React JS',
+        image: `${react}`,
+      },
+    ],
+    deployLink: '',
+    image: '',
+  },
+  {
+    title: 'CV',
+    summary: '',
+    technologies: [
+      {
+        name: 'HTML',
+        image: `${html}`,
+      },
+      {
+        name: 'CSS',
+        image: `${css}`,
+      },
+    ],
     deployLink: '',
     image: '',
   },
@@ -81,31 +170,11 @@ const filter = [
     image: `${reactjs}`,
   },
   {
-    name: 'Redux',
-    image: `${redux}`,
-  },
-  {
     name: 'Node JS',
     image: `${nodejs}`,
   },
   {
     name: 'Express JS',
     image: `${express}`,
-  },
-  {
-    name: 'Ant Design',
-    image: `${antDesign}`,
-  },
-  {
-    name: 'Bootstrap',
-    image: `${bootstrap}`,
-  },
-  {
-    name: 'PostgreSQL',
-    image: `${postgre}`,
-  },
-  {
-    name: 'Sequelize JS',
-    image: `${sequelize}`,
   },
 ];
