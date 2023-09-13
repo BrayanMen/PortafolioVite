dc40316270e4db3414b37c0e1deec44a3f3eac4a
BrayanMen <95663591+BrayanMen@users.noreply.github.com>
2023-09-13 19:24:11 -0300

    3 files modified

--- src/Component/ModalDetail/ModalDetail.jsx
+++ src/Component/ModalDetail/ModalDetail.jsx
@@ -1,29 +1,51 @@
-import Modal from './Modal'
+import Modal from './Modal';
 import styles from './ModalDetail.module.scss';
+// import git from '../../assets/Images/Icons/github-fill.png';
+// import deploy from '../../assets/Images/Icons/deploy.png';
 import PropTypes from 'prop-types';
 
-export default function ModalDetail({selectedProject,closeModal, isOpen}) {
-    ModalDetail.propTypes = {
-        isOpen: PropTypes.bool.isRequired,    
-        selectedProject: PropTypes.array.isRequired,
-        closeModal: PropTypes.func.isRequired
-    };
+export default function ModalDetail({ selectedProject, closeModal, isOpen }) {
+  ModalDetail.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    selectedProject: PropTypes.array.isRequired,
+    closeModal: PropTypes.func.isRequired,
+  };
   return (
-    <Modal closeModal={closeModal} isOpen={isOpen} selectedProject={selectedProject}>
-         {selectedProject && (
-                <div className={styles.detailContainer}>
-                  <img className={styles.detailImg} src={selectedProject.image} alt="" />
-                  <p className={styles.pdetail}>{selectedProject.summary}</p>
-                  <p className={styles.detailPT}>
-                    Tecnologías:
-                    </p>
-                    <p>
-                    {selectedProject.technologies.map((t , index) => (
-                      <img className={styles.ptimg} key={index} src={t.image} alt="" width={'50px'} />
-                    ))}</p>
-                  <a href={selectedProject.deployLink}>Enlace al despliegue</a>
-                </div>
-              )}
+    <Modal
+      closeModal={closeModal}
+      isOpen={isOpen}
+      selectedProject={selectedProject}
+    >
+      {selectedProject && (
+        <div className={styles.detailContainer}>
+          <img
+            className={styles.detailImg}
+            src={selectedProject.image}
+            alt=""
+          />
+          <p className={styles.pdetail}>{selectedProject.summary}</p>
+          <p className={styles.detailPT}>Tecnologías:</p>
+          <p>
+            {selectedProject.technologies.map((t, index) => (
+              <img
+                className={styles.ptimg}
+                key={index}
+                src={t.image}
+                alt=""
+                width={'50px'}
+              />
+            ))}
+          </p>
+          <div>
+            <a href={selectedProject.deployLink.url} className={styles.deployLink}>
+              <img src={selectedProject.deployLink.image} alt="deploy" width={'30px'}/> Deploy
+            </a>
+            <a href={selectedProject.githubLink.url} className={styles.deployLink}>
+              <img src={selectedProject.githubLink.image} alt="github" width={'30px'} /> Github
+            </a>
+          </div>
+        </div>
+      )}
     </Modal>
-  )
+  );
 }
--- src/Data/projects.jsx
+++ src/Data/projects.jsx
@@ -11,12 +11,14 @@ import sequelize from '../assets/Images/Icons/sequelize.png';
 import pf from '../assets/Images/PF.jpg';
 import clima from '../assets/Images/WeatherApp.png';
 import todo from '../assets/Images/todo.jpeg';
-
+import github from '../assets/Images/Icons/github-fill.png';
+import deploy from '../assets/Images/Icons/deploy.png';
 
 export const projectsData = [
   {
     title: 'Museo Patagonico de Ciencias Naturales "Juan Carlos Salgado"',
-    summary: "Este proyecto inicialmente concebido como el proyecto final para el Bootcamp Henry, se ha convertido en una implementación real para el Museo de Ciencias Naturales de Río Negro. La plataforma cuenta con una amplia gama de funcionalidades que incluyen una página de eventos, una pasarela de pago, un panel de control tanto para administradores como para usuarios, una base de datos sólida, filtros avanzados, y muchas otras características.",
+    summary:
+      'Este proyecto inicialmente concebido como el proyecto final para el Bootcamp Henry, se ha convertido en una implementación real para el Museo de Ciencias Naturales de Río Negro. La plataforma cuenta con una amplia gama de funcionalidades que incluyen una página de eventos, una pasarela de pago, un panel de control tanto para administradores como para usuarios, una base de datos sólida, filtros avanzados, y muchas otras características.',
     technologies: [
       {
         name: 'Javascript',
@@ -51,7 +53,14 @@ export const projectsData = [
         image: `${sequelize}`,
       },
     ],
-    deployLink: 'https://pf-museo-front-end-pf09henry.vercel.app/',
+    deployLink: {
+      url: 'https://pf-museo-front-end-pf09henry.vercel.app/',
+      image: `${deploy}`,
+    },
+    githubLink: {
+      url: 'https://github.com/Pf09Henry/Pf_Museo_FrontEnd/',
+      image: `${github}`,
+    },
     image: `${pf}`,
   },
   {
@@ -83,12 +92,20 @@ export const projectsData = [
         image: `${sequelize}`,
       },
     ],
-    deployLink: '',
+    deployLink: {
+      url: '',
+      image: `${deploy}`,
+    },
+    githubLink: {
+      url: '',
+      image: `${github}`,
+    },
     image: '',
   },
   {
     title: 'ToDo List',
-    summary: "Aplicación de registro de tareas desarrollada en ReactJS, que emplea conceptos de persistencia de datos mediante localStorage. Permite gestionar tareas, incluyendo la creación, eliminación y marcado como completadas, todo ello gestionado a través de estados.",
+    summary:
+      'Aplicación de registro de tareas desarrollada en ReactJS, que emplea conceptos de persistencia de datos mediante localStorage. Permite gestionar tareas, incluyendo la creación, eliminación y marcado como completadas, todo ello gestionado a través de estados.',
     technologies: [
       {
         name: 'Javascript',
@@ -99,7 +116,14 @@ export const projectsData = [
         image: `${reactjs}`,
       },
     ],
-    deployLink: 'https://brayanmen.github.io/TodoList/',
+    deployLink: {
+      url: 'https://brayanmen.github.io/TodoList/',
+      image: `${deploy}`,
+    },
+    githubLink: {
+      url: 'https://github.com/BrayanMen/TodoList',
+      image: `${github}`,
+    },
     image: `${todo}`,
   },
   {
@@ -119,12 +143,20 @@ export const projectsData = [
         image: `${css}`,
       },
     ],
-    deployLink: '',
+    deployLink: {
+      url: '',
+      image: `${deploy}`,
+    },
+    githubLink: {
+      url: '',
+      image: `${github}`,
+    },
     image: '',
   },
   {
     title: 'Weather App',
-    summary: 'Weather App construida con React.js, que implementa conceptos de modularización, llamadas a APIs, pasaje de Props, ciclos de vida y uso de estados.',
+    summary:
+      'Weather App construida con React.js, que implementa conceptos de modularización, llamadas a APIs, pasaje de Props, ciclos de vida y uso de estados.',
     technologies: [
       {
         name: 'Javascript',
@@ -135,12 +167,20 @@ export const projectsData = [
         image: `${reactjs}`,
       },
     ],
-    deployLink: "https://weather-52j1nztdj-brayanmen.vercel.app/",
+    deployLink: {
+      url: 'https://weather-52j1nztdj-brayanmen.vercel.app/',
+      image: `${deploy}`,
+    },
+    githubLink: {
+      url: 'https://github.com/BrayanMen/Weather-App',
+      image: `${github}`,
+    },
     image: `${clima}`,
   },
   {
     title: 'CV',
-    summary: "Curriculum Vitae creado utilizando HTML y CSS para fortalecer mis habilidades en desarrollo web.",
+    summary:
+      'Curriculum Vitae creado utilizando HTML y CSS para fortalecer mis habilidades en desarrollo web.',
     technologies: [
       {
         name: 'HTML',
@@ -151,7 +191,14 @@ export const projectsData = [
         image: `${css}`,
       },
     ],
-    deployLink: 'https://cv-chi-red.vercel.app/',
+    deployLink: {
+      url: 'https://cv-chi-red.vercel.app/',
+      image: `${deploy}`,
+    },
+    githubLink: {
+      url:  'https://github.com/BrayanMen/cv',
+      image: `${github}`,
+    },
     image: '',
   },
 ];
@@ -159,7 +206,7 @@ export const projectsData = [
 export const filterTech = [
   {
     name: 'Todos',
-    image: ''
+    image: '',
   },
   {
     name: 'Javascript',
@@ -182,4 +229,3 @@ export const filterTech = [
     image: `${nodejs}`,
   },
 ];
-
