b770c9aeb72655516c4b20bc8cd810810a596222
BrayanMen <brayanjmr880@gmail.com>
2023-09-14 11:38:26 -0300

    fixed projects

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -13,41 +13,8 @@ export default function Projects() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [filterT, setFilterT] = useState('Todos');
   const [pages, setPages] = useState(1);
-  const projectPerPage = 2;
+  const projectPerPage = 4;
 
-  const images = projectsData.map((p) => p.image);
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [imageSel, setImageSele] = useState(images[0]);
-
-  useEffect(() => {
-    if (props.autoPlay || !props.showButtons) {
-      const interval = setInterval(() => {
-        newImage(slideIndex, images);
-      }, 5000);
-      return () => clearInterval(interval);
-    }
-  }, [slideIndex, images, props.autoPlay, props.showButtons]);
-
-  const newImage = (index, images, next = true) => {
-    const condition = next ? slideIndex < images.length - 1 : slideIndex > 0;
-    const nextIndex = next
-      ? condition
-        ? slideIndex + 1
-        : 0
-      : condition
-      ? slideIndex - 1
-      : images.length - 1;
-    setImageSele(images[nextIndex]);
-    setSlideIndex(nextIndex);
-  };
-
-  const previus = () => {
-    newImage(slideIndex, images, false);
-  };
-
-  const next = () => {
-    newImage(slideIndex, images);
-  };
 
   const hanldlerFilter = (fill) => {
     setFilterT(fill);
@@ -57,8 +24,8 @@ export default function Projects() {
   const filteredProjects =
     filterT !== 'Todos'
       ? projectsData.filter((project) =>
-          project.technologies.some((tech) => tech.name === filterT)
-        )
+        project.technologies.some((tech) => tech.name === filterT)
+      )
       : projectsData;
 
   const openModal = (project) => {
@@ -105,32 +72,24 @@ export default function Projects() {
                   Detalles
                 </button>
               </h3>
-              {/* <p className={styles.pCard}>
-                {p.technologies.map((t) => (
-                  <img src={t.image} alt="" width={'40px'} />
-                ))}
-              </p> */}
-              {/* <a href={p.deployLink}>
-                <h4>Enlace</h4>
-              </a> */}
             </div>
           ))}
-          <div>
-            <ul className={styles.pagination}>
-              {Array.from({
-                length: Math.ceil(filteredProjects.length / projectPerPage),
-              }).map((_, index) => (
-                <li key={index}>
-                  <button
-                    className={styles.btnPagination}
-                    onClick={() => paginate(index + 1)}
-                  >
-                    {index + 1}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
+        </div>
+        <div>
+          <ul className={styles.pagination}>
+            {Array.from({
+              length: Math.ceil(filteredProjects.length / projectPerPage),
+            }).map((_, index) => (
+              <li key={index}>
+                <button
+                  className={styles.btnPagination}
+                  onClick={() => paginate(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
 
         {isModalOpen && (
@@ -141,23 +100,6 @@ export default function Projects() {
             isOpen={isModalOpen}
           />
         )}
-      </div>
-
-      <div className={styles.divCarrosel}>
-        <div className={styles.projectCarrosel}>
-          <button className="proyeButton" onClick={previus}>
-            {'<'}
-          </button>
-          <img
-            src={imageSel}
-            alt="default"
-            className={styles.projectCarroselImg}
-            autoPlay={false}
-          />
-          <button className="proyeButton" onClick={next}>
-            {'>'}
-          </button>
-        </div>
       </div>
     </section>
   );
